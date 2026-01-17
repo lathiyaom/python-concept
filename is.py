759c5f2d4e2e2c175cf3ed43e59ba7dd32586855
Om Lathiya <lathiya52725@gmail.com>
2026-01-17 15:02:26 +0530

    Comparisan concept

---
+++ is.py
@@ -0,0 +1,24 @@
+# # # a = [1, 2, 3]
+# # # b = [1, 2, 3]
+# # # print(a is b)
+# # #
+# #
+# # a = 10
+# # b = 10
+# # b = b + 1
+# # print(a == b)
+# # print(a is b, id(a), id(b))
+#
+# a = 1000
+# b = 1000
+#
+# print(a == b)  # True
+# print(a is b)  #  False (sometimes)
+a = [1, 2]
+b = a
+c = [1, 2]
+
+print(a == b)  # ?
+print(a is b)  # ?
+print(a == c)  # ?
+print(a is c)  # ?
